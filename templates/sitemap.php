d9a1069a4240377150877f5cead4f450e3fccbdc
james.barnard <james.barnard@readingroom.com>
2015-10-16 15:20:20 +0100

    adding example sitemap function and example template attribute function

--- templates/sitemap.php
+++ templates/sitemap.php
@@ -1,9 +1,7 @@
 <nav class="sitemap">
-	<h2>Sitemap</h2>
-	
 	<!-- Example of getting an attribute from the global template object (things like uri, title etc. See template-class.php for options) -->
-	{{ tp_attr('template') }}
+	<h2>{{ tp_attr('title') }}</h2>
 	
-	<!-- Example of getting the full sitemap -->
+	<!-- Example of getting the full sitemap (boolean parameter if you want to only show items that have existing template files) -->
 	{{ sitemap(true) }}
 </nav>
