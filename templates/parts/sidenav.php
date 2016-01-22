54852ca9072a244eb1be2c355aa25a66279776b0
james.barnard <james.barnard@readingroom.com>
2016-01-22 10:08:12 +0000

    checking if any children before showing sidenav

--- templates/parts/sidenav.php
+++ templates/parts/sidenav.php
@@ -2,13 +2,15 @@
 	<?php //TODO: Work on integrating methods for sidenav stuff into base class ?>
 	
 	<?php // Example of getting the sidenav of the overall top level parent ?>
-	<?php echo $tp->build_nav($tp->structure[$tp->uri_structure[0]]['children'], $tp->build_uri($tp->uri_structure[0])); ?>
+	<?php if (isset($tp->structure[$tp->uri_structure[0]]['children'])): ?>
+	    <?php echo $tp->build_nav($tp->structure[$tp->uri_structure[0]]['children'], $tp->build_uri($tp->uri_structure[0])); ?>
+	<?php endif; ?>
 	
 	<?php // example of checking if parent has children and if so showing direct parent children ?>
-	<?php if (isset($template->parent->page['children']) && !empty($template->parent->page['children'])): ?>
+	<?php if (isset($tp->parent->page['children']) && !empty($tp->parent->page['children'])): ?>
 		<nav class="sidenav">
-			<h2 class="sidenav__parent"><a href="<?php echo $template->build_uri($template->parent->uri); ?>"><?php echo $template->get_parent_title(); ?></a></h2>
-			<?php $template->build_nav($template->parent->page['children'], $template->build_uri($template->get_parent_uri())); ?>
+			<h2 class="sidenav__parent"><a href="<?php echo $tp->build_uri($tp->parent->uri); ?>"><?php echo $tp->get_parent_title(); ?></a></h2>
+			<?php $tp->build_nav($tp->parent->page['children'], $tp->build_uri($tp->get_parent_uri())); ?>
 		</nav>
 	<?php endif; ?>
 </aside>
